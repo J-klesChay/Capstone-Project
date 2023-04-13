d25fab9bc765ef54c33e2fd3b691d5c254c5ca7d
J-klesChay <130422317+J-klesChay@users.noreply.github.com>
2023-04-13 04:29:01 +0000

    edited storage.py and front.py for real

---
+++ storage.py
@@ -0,0 +1,18 @@
+class Collection:
+    def __init__():
+        pass
+
+class StudentCollection(Collection):
+    pass
+
+class CCACollection(Collection):
+    pass
+
+class SubjectCollection(Collection):
+    pass
+
+class ClassCollection(Collection):
+    pass
+
+class ActivityCollection(Collection):
+    pass
